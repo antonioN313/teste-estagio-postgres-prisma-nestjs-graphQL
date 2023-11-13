ce579f8d5be8ec6edaaede21da8d5d226c0023be
antonioN313 <antoniocarlosfilho2003@gmail.com>
2023-11-13 16:30:34 -0300

    update: import @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriasModule } from './categorias/categorias.module';
 import { ProdutosModule } from './produtos/produtos.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [CategoriasModule, ProdutosModule],
+  imports: [
+    CategoriasModule,
+    ProdutosModule,
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
